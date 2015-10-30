a5f0f62f40b84a2b980ff02b3258d6fdba17d9d4
memu-mani <memumani@gmail.com>
2015-10-30 08:33:53 -0700

    Service

---
+++ IService1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VerizonRepairService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IService1
+    {               
+
+        [WebInvoke(Method = "GET",
+               BodyStyle = WebMessageBodyStyle.WrappedRequest,
+               RequestFormat = WebMessageFormat.Json,
+               ResponseFormat = WebMessageFormat.Json,
+               UriTemplate = "GetRepairTechnician/{0}")]
+        [OperationContract]
+        string GetRepairTechnician(string problemType);
+
+        [WebInvoke(Method = "GET",
+              BodyStyle = WebMessageBodyStyle.WrappedRequest,
+              RequestFormat = WebMessageFormat.Json,
+              ResponseFormat = WebMessageFormat.Json,
+              UriTemplate = "GetInstallTechnician")]
+        [OperationContract]
+        string GetInstallTechnician();
+
+
+        [WebInvoke(Method = "GET",
+              BodyStyle = WebMessageBodyStyle.WrappedRequest,
+              RequestFormat = WebMessageFormat.Json,
+              ResponseFormat = WebMessageFormat.Json,
+              UriTemplate = "GetCustomerLocation")]
+        [OperationContract]
+        string GetCustomerLocation();
+
+        
+    }
+
+
+    [DataContract]
+    public class Technician
+    {
+        [DataMember]
+        public string Name;
+        [DataMember]
+        public string Lang;
+        [DataMember]
+        public string Lat;
+        [DataMember]
+        public string skill;
+
+    }
+
+    [DataContract]
+    public class Customer
+    {
+        [DataMember]
+        public string Name;
+        [DataMember]
+        public string Lang;
+        [DataMember]
+        public string Lat;
+        [DataMember]
+        public string ServiceType;
+
+    }
+
+}
---
+++ Service1.svc.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Xml;
+
+namespace VerizonRepairService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Service1 : IService1
+    {
+        //    public string GetData(int value)
+        //    {
+        //        return string.Format("You entered: {0}", value);
+        //    }
+
+        //    public CompositeType GetDataUsingDataContract(CompositeType composite)
+        //    {
+        //        if (composite == null)
+        //        {
+        //            throw new ArgumentNullException("composite");
+        //        }
+        //        if (composite.BoolValue)
+        //        {
+        //            composite.StringValue += "Suffix";
+        //        }
+        //        return composite;
+        //    }
+        public string GetRepairTechnician(string problemType)
+        {
+            try
+            {
+                List<Technician> technicianList = new List<Technician>();
+
+                technicianList.Add(new Technician { Name = "Technician 1", Lat = "33.84659", Lang = "-84.35686", skill = "equipment service" });
+                technicianList.Add(new Technician { Name = "Technician 2", Lat = "33.846553", Lang = "-84.35886", skill = "equipment service" });
+                technicianList.Add(new Technician { Name = "Technician 3", Lat = "33.846653", Lang = "-84.366125", skill = "equipment service" });
+
+                DataContractJsonSerializer serializer = new DataContractJsonSerializer(technicianList.GetType());
+                MemoryStream memoryStream = new MemoryStream();
+                serializer.WriteObject(memoryStream, technicianList);
+
+
+                string json = Encoding.Default.GetString(memoryStream.ToArray());
+                return json;
+
+            }catch( Exception  ex)
+            {
+                return "No Technician availabe";
+            }                     
+        }
+        public string GetInstallTechnician()
+        {
+            try
+            {
+                List<Technician> technicianList = new List<Technician>();
+
+                //technicianList.Add(new Technician { Name = "Technician 1", Lat = "33.84659", Lang = "-84.35686", skill = "equipment service" });
+                technicianList.Add(new Technician { Name = "Technician 2", Lat = "33.846553", Lang = "-84.35886", skill = "equipment service" });
+              //  technicianList.Add(new Technician { Name = "Technician 3", Lat = "33.846653", Lang = "-84.366125", skill = "equipment service" });
+
+                DataContractJsonSerializer serializer = new DataContractJsonSerializer(technicianList.GetType());
+                MemoryStream memoryStream = new MemoryStream();
+                serializer.WriteObject(memoryStream, technicianList);
+
+
+                string json = Encoding.Default.GetString(memoryStream.ToArray());
+                return json;
+
+            }
+            catch (Exception ex)
+            {
+                return "No Technician availabe";
+            }
+        }
+
+        public string GetCustomerLocation()
+        {
+            try
+            {
+                List<Customer> customer = new List<Customer>();
+
+
+                customer.Add(new Customer { Name = "Your location", Lat = "33.846553", Lang = "-84.35886", ServiceType = "TV - DATA -FDV" });
+            
+
+                DataContractJsonSerializer serializer = new DataContractJsonSerializer(customer.GetType());
+                MemoryStream memoryStream = new MemoryStream();
+                serializer.WriteObject(memoryStream, customer);
+
+
+                string json = Encoding.Default.GetString(memoryStream.ToArray());
+                return json;
+
+            }
+            catch (Exception ex)
+            {
+                return "No Service at availabe";
+            }
+        }
+    }
+}
+
